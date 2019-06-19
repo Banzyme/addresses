69fd2de78cd379764474943fe3d065c784335d95
N Nemakhavhani <nbanzyme@gmail.com>
2019-06-20 01:21:56 +0200

    Try swagger UI

--- source/express-api/src/app.ts
+++ source/express-api/src/app.ts
@@ -2,11 +2,17 @@ import express  =  require('express');
 import { AppRoutesConfig } from './routes';
 
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./../swagger.json');
+
 app.set("PORT", process.env.PORT || 3001);
 const routes = new AppRoutesConfig(app);
+
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // test route
-app.get('/', (request, response)=>{
-    response.send("Address API is live..!");
-});
+// app.get('/', (request, response)=>{
+//     response.send("Address API is live!");
+// });
 
 export default app;
